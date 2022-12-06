78690e7e0875d691f237f8f7de8f7b3b1f821f9d
Gleb torbin <glebb0@admins-MacBook-Pro.local>
2022-12-05 18:31:22 -0800

    perform admin to create quizes

--- app/quiz/admin.py
+++ app/quiz/admin.py
@@ -1,9 +1,17 @@
 from django.contrib import admin
 
-from .models import Quiz
+from .models import Quiz, Question, Answer
 
+class AnswersInLine(admin.TabularInline):
+    model = Answer
+    extra = 4
+
+class QuestionInLine(admin.TabularInline):
+    inlines = [AnswersInLine]
+    model = Question
 
 class QuizAdmin(admin.ModelAdmin):
+    inlines = [QuestionInLine, AnswersInLine]
     list_display = ('pk', 'title', 'description', 'is_active',)
     list_editable = ('is_active',)
     search_fields = ('title',)
@@ -11,5 +19,8 @@ class QuizAdmin(admin.ModelAdmin):
     empty_value_display = '-пусто-'
 
 
+
+
 admin.site.register(Quiz, QuizAdmin)
+
 
--- app/quiz/models.py
+++ app/quiz/models.py
@@ -17,15 +17,24 @@ class Question(models.Model):
     quiz = models.ForeignKey(Quiz, verbose_name='Quiz',
                              on_delete=models.CASCADE,
                              related_name='questions')
-    text = models.TextField('Text')
+    text = models.CharField('Text', max_length=555)
+
+    def __str__(self):
+        return self.text
 
 
 class Answer(models.Model):
+    quiz = models.ForeignKey(Quiz, verbose_name='Quiz',
+                                 on_delete=models.CASCADE,
+                                 related_name='quizes_answers')
     question = models.ForeignKey(Question, verbose_name='Question',
                                  on_delete=models.CASCADE,
                                  related_name='answers')
-    text = models.TextField('Text')
+    text = models.CharField('Text', max_length=455)
     is_true = models.BooleanField('Is true', default=False)
+
+    def __str__(self):
+        return self.text
 
 
 class Result(models.Model):
