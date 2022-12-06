2e00c86316c40ada9cad22eaed0af05ff5f26ed4
Gleb torbin <glebb0@admins-MacBook-Pro.local>
2022-12-06 15:58:06 -0800

    ok

--- app/quiz/views.py
+++ app/quiz/views.py
@@ -1,5 +1,12 @@
-from django.shortcuts import render
-from .models import Quiz
+from django.shortcuts import render, redirect, get_object_or_404
+from .models import Quiz, Answer
+from django.contrib.auth.decorators import login_required
+from .forms import QuizForm
+from .models import (
+    Question,
+    Quiz,
+    Answer
+)
 
 def index(request):
     quiz_list = Quiz.objects.all()
@@ -8,5 +15,17 @@ def index(request):
     }
     return render(request, 'quiz/index.html', context)
 
+
 def quiz_passing(request, quiz_id):
-    pass
+    quiz = Quiz.objects.get(id = quiz_id)
+    questions = Question.objects.filter(quiz=quiz)
+    answers = Answer.objects.filter(quiz=quiz, question=questions)
+    if request.method == "POST":
+        form = QuizForm(quiz_id, request.POST or None)
+        if form.is_valid():
+            ans = form.save(commit=False)
+            ans.save()
+            return redirect('quiz:index')
+    else:
+        form = QuizForm(quiz_id)
+    return render(request, 'quiz/quiz.html', {'form': form, 'quiz': quiz, 'questions': questions, 'answers': answers})
